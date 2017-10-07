bc77f92eeb9b9cb2cc9fe4f583dfe7d18d8a6ecd
Peter <a184999@gmail.com>
2017-10-07 17:34:31 +0800

    [Update]much stuff

--- CreateDockerNode.py
+++ CreateDockerNode.py
@@ -4,7 +4,31 @@ import uuid
 print "Insert how many node you want to create"
 numberOfDockerNode = input()
 print "Create "+str(numberOfDockerNode)+" node"
-with open("docker-compose.yml", "a") as myfile:
+with open("docker-compose.yml", "w") as myfile:
+    myfile.write("version: '2'")
+    myfile.write("\n")
+    myfile.write("services:")
+    myfile.write("\n")
+    myfile.write(" bitcoindev:")
+    myfile.write("\n")
+    myfile.write("  build: BitcoinDev")
+    myfile.write("\n")
+    myfile.write("  image: bitcoindev")
+    myfile.write("\n")
+    myfile.write(" a:")
+    myfile.write("\n")
+    myfile.write("  build: MasterBaseImage")
+    myfile.write("\n")
+    myfile.write("  image: bitcoinmnode")
+    myfile.write("\n")
+    myfile.write("  ports:")
+    myfile.write("\n")
+    myfile.write("   - 20000:18332")
+    myfile.write("\n")
+    myfile.write("  depends_on:")
+    myfile.write("\n")
+    myfile.write("  - bitcoindev")
+    myfile.write("\n")
     i=0
     while i < numberOfDockerNode:
         portsNumber = 20001+i
